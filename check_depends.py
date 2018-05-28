dae849e748863c55e0f89ec54b44c1e32a7688f2
MattLimb <matt.limb17@gmail.com>
2018-05-28 12:58:54 +0100

    Re-organisation and Completed check_depends.py

--- check_depends.py
+++ check_depends.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 
+import os, subprocess
+
+def main(*args):
+    for x in args:
+        try:
+            print("Checking " + str(x) + "...")
+            __import__(x)
+        except ModuleNotFoundError:
+            command = "pip3 install " + str(x)
+            subprocess.Popen(command, shell=True, executable='/bin/bash').wait()
+
+
+
+if os.name == "posix":
+    f = open(".depends/depends.txt", "r")
+    dep = f.readlines()
+    f.close()
+    for x in dep:
+        main(x.strip("\n"))
+    
+else:
+    print("I cannot run on " + os.name + ". Sorry!")
